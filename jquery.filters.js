7013e1782ac4b78ad45d7a9fcec661090ef9cd52
Ohad Perry <ohadpartuck@gmail.com>
2015-11-11 15:03:15 +0200

    shift_select_self_implement

--- jquery.filters.js
+++ jquery.filters.js
@@ -987,6 +987,28 @@
         return (-1 < $.inArray(item, array))
     }
 
+    $.fn.shiftSelectable = function() {
+        var lastChecked,
+            $boxes = this;
+
+        $boxes.click(function(evt) {
+            if(!lastChecked) {
+                lastChecked = this;
+                return;
+            }
+
+            if(evt.shiftKey) {
+                var start = $boxes.index(this),
+                    end = $boxes.index(lastChecked);
+                $boxes.slice(Math.min(start, end), Math.max(start, end) + 1)
+                    .attr('checked', lastChecked.checked)
+                    .trigger('change');
+            }
+
+            lastChecked = this;
+        });
+    };
+
     /////////////// Events Binding end  ////////////////////
 
 }( jQuery ));
